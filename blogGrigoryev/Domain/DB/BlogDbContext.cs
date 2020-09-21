0abf339f4445b70d31e76470d4ab5b3c5b502f8a
Григорьев Александр <foxamurai@gmail.com>
2020-09-21 19:58:23 +0400

    to be continued

--- blogGrigoryev/Domain/DB/BlogDbContext.cs
+++ blogGrigoryev/Domain/DB/BlogDbContext.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using blogGrigoryev.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace blogGrigoryev.Domain.DB
 {
-    public class BlogDbContext
+    public class BlogDbContext : IdentityDbContext<User, IdentityRole<int>, int>
     {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Пользователи
+        /// </summary>
+        public override DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// Сотрудники
+        /// </summary>
+        public DbSet<Employee> Employees { get; private set; } 
+
+        /// <summary>
+        /// Пост блога
+        /// </summary>
+        public DbSet<BlogPost> BlogPosts { get; private set; }
+
+        /// <inheritdoc/>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(x =>
+            {
+                x.HasOne(y => y.Employee)
+                .WithOne()
+                .HasForeignKey<User>("EmployeeId")
+                .IsRequired(true);
+                x.HasIndex("EmployeeId").IsUnique(true);
+            });
+        }
+
+
     }
 }
 
